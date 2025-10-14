9dc1c9239a703d3acaffb8267b54553429e2c524
Vlekzander <alexandrepierret@protonmail.com>
2025-10-14 12:45:45 +0200

    feat(image): Add fill_image function

--- src/image/fill_image.c
+++ src/image/fill_image.c
@@ -1,28 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   image.h                                            :+:      :+:    :+:   */
+/*   fill_image.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: apierret <apierret@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/13 16:42:16 by apierret          #+#    #+#             */
-/*   Updated: 2025/10/14 12:42:16 by apierret         ###   ########.fr       */
+/*   Created: 2025/10/14 12:42:34 by apierret          #+#    #+#             */
+/*   Updated: 2025/10/14 12:45:25 by apierret         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef IMAGE_H
-# define IMAGE_H
+#include <stddef.h>
+#include "image.h"
 
-typedef struct s_image
+void	fill_image(t_image *image, int color)
 {
-	int	width;
-	int	height;
-	int	*pixels;
-}	t_image;
+	int	i;
+	int	size;
 
-t_image	*create_image(int width, int height);
-void	free_image(t_image *image);
-void	put_pixel(t_image *image, int x, int y, int color);
-void	fill_image(t_image *image, int color);
-
-#endif
+	if (image == NULL || image->pixels == NULL)
+		return ;
+	size = image->width * image->height;
+	i = 0;
+	while (i < size)
+	{
+		image->pixels[i] = color;
+		i++;
+	}
+}
