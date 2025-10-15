fdab6e1f14317db8235daa7352337837f3592603
Vlekzander <alexandrepierret@protonmail.com>
2025-10-15 11:59:32 +0200

    feat(shapes): add draw_rect with fill mode and clipping

---
+++ include/shapes.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   shapes.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apierret <apierret@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/15 11:29:38 by apierret          #+#    #+#             */
+/*   Updated: 2025/10/15 11:34:51 by apierret         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SHAPES_H
+# define SHAPES_H
+# include "image.h"
+
+typedef struct s_rect
+{
+	int	x;
+	int	y;
+	int	width;
+	int	height;
+}	t_rect;
+
+void	draw_rect(t_image *image, t_rect rect, int fill, int color);
+
+#endif
---
+++ src/shapes/draw_rect.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   draw_rect.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apierret <apierret@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/15 11:34:25 by apierret          #+#    #+#             */
+/*   Updated: 2025/10/15 11:57:18 by apierret         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+#include "image.h"
+#include "shapes.h"
+
+static void	prepare_rect(t_rect *rect)
+{
+	if (rect == NULL)
+		return ;
+	if (rect->width < 0)
+	{
+		rect->width = -rect->width;
+		rect->x -= rect->width;
+	}
+	if (rect->height < 0)
+	{
+		rect->height = -rect->height;
+		rect->y -= rect->height;
+	}
+	if (rect->x < 0)
+	{
+		rect->width += rect->x;
+		rect->x = 0;
+	}
+	if (rect->y < 0)
+	{
+		rect->height += rect->y;
+		rect->y = 0;
+	}
+}
+
+void	draw_rect(t_image *image, t_rect rect, int fill, int color)
+{
+	int	i;
+	int	j;
+
+	if (image == NULL || rect.width == 0 || rect.height == 0)
+		return ;
+	prepare_rect(&rect);
+	if (rect.width <= 0 || rect.height <= 0)
+		return ;
+	i = rect.y;
+	while (i < rect.y + rect.height && i < image->height)
+	{
+		j = rect.x;
+		while (j < rect.x + rect.width && j < image->width)
+		{
+			if (fill || (i == rect.y || j == rect.x
+					|| i == rect.y + rect.height -1
+					|| j == rect.x + rect.width -1))
+				put_pixel(image, j, i, color);
+			j++;
+		}
+		i++;
+	}
+}
