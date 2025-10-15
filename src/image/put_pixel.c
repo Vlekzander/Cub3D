f13c30323fb0a3ee672f0b8c371a324768cb3c50
Vlekzander <alexandrepierret@protonmail.com>
2025-10-15 15:49:57 +0200

    Fix(put_pixel): Adding boundaries to prevent drawing outside the image

--- src/image/put_pixel.c
+++ src/image/put_pixel.c
@@ -6,7 +6,7 @@
 /*   By: apierret <apierret@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 12:37:35 by apierret          #+#    #+#             */
-/*   Updated: 2025/10/15 12:33:27 by apierret         ###   ########.fr       */
+/*   Updated: 2025/10/15 15:49:00 by apierret         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,8 @@
 
 void	put_pixel(t_image *image, int x, int y, int color)
 {
-	if (image == NULL)
+	if (image == NULL || x < 0 || y < 0
+		|| x >= image->width || y >= image->height)
 		return ;
 	image->pixels[y * image->width + x] = color;
 }
