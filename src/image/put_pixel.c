400a09ff99522f5a27ff16cafd2fb08d34e8a97c
Vlekzander <alexandrepierret@protonmail.com>
2025-11-27 23:55:07 +0100

    fix: Simplification of put_pixel

--- src/image/put_pixel.c
+++ src/image/put_pixel.c
@@ -6,7 +6,7 @@
 /*   By: apierret <apierret@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 12:37:35 by apierret          #+#    #+#             */
-/*   Updated: 2025/10/15 15:49:00 by apierret         ###   ########.fr       */
+/*   Updated: 2025/11/27 23:54:23 by apierret         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,11 @@
 
 void	put_pixel(t_image *image, int x, int y, int color)
 {
-	static int	old_base_color = 0;
-	static int	old_color = 0;
-	static int	blended = 0;
-	int			base_color;
+	int	*cell;
 
 	if (image == NULL || x < 0 || y < 0
 		|| x >= image->width || y >= image->height)
 		return ;
-	base_color = image->pixels[y * image->width + x];
-	if (old_color != color || old_base_color != base_color)
-	{
-		old_color = color;
-		old_base_color = base_color;
-		blended = blend_colors(base_color, color);
-	}
-	image->pixels[y * image->width + x] = blended;
+	cell = image->pixels + (y * image->width + x);
+	*cell = blend_colors(*cell, color);
 }
